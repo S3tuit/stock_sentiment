3b64d0b458562da5d92776d891c5149d617edf0f
Matteo Ladislai lp <matteo.ladislai@hotmail.com>
2024-08-31 17:05:50 +0200

    updated the dags, removed async

--- dags/yahoo_extract.py
+++ dags/yahoo_extract.py
@@ -1,9 +1,7 @@
-
 from datetime import datetime, timedelta
 import logging
 from bs4 import BeautifulSoup
-import aiohttp
-import asyncio
+import requests
 
 from airflow.decorators import dag, task
 from airflow.models import Variable
@@ -12,17 +10,14 @@ from helper.models import Article
 from helper.kafka_produce import make_producer, ProducerCallback
 from helper import schemas
 
-
-
 # Constants for Kafka and API configurations
 TOPIC_NAME = "test.articles"
 SCHEMA_REGISTRY_URL = Variable.get("SCHEMA_REGISTRY_URL")
 BOOTSTRAP_SERVERS = Variable.get("BOOTSTRAP_SERVERS")
 
-
 # Constants for scraping
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/127.0.0.0'
 }
 COOKIES = {
     'GUC': 'AQABCAFmwcNm80IfWgSU&s=AQAAAGoqDQ7v&g=ZsB0FA',
@@ -32,22 +27,11 @@ COOKIES = {
     'PRF': 't%3DACMR%26newChartbetateaser%3D0%252C1725284010825'
 }
 
-
-tickets = ['ACMR', 'RIOT']
-
+tickets = ['ACMR', 'RIOT', 'HAL']
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-
-# Gets the text content of the page
-async def fetch_page(session, url):
-    async with session.get(url) as response:
-        response.raise_for_status()
-        return await response.text()
-
-
 
 @dag(
     schedule=None,
@@ -61,59 +45,54 @@ def yahoo_extract():
     and produce it to a Kafka topic.
     """
 
-
     @task(
         task_id='get_the_links',
         retries=0,
         retry_delay=timedelta(seconds=5)
     )
     def get_news_links_task(tickets):
-        async def async_get_news_links_task(tickets):
-            """
-            Task to retrieve news links for specific stocks from Yahoo News.
-            
-            Args:
-                tickets (list): The stock ticker symbol.
-            
-            Returns:
-                list: A list of dictionaries containing article metadata.
-            """
-            raw_articles = []
-            
-            async with aiohttp.ClientSession(headers=HEADERS, cookies=COOKIES) as session:
-                for ticket in tickets:
-                    url = f'https://finance.yahoo.com/quote/{ticket}/news/'
-                    
-                    # Fetch the main page
-                    page_content = await fetch_page(session, url)
-                    if not page_content:
-                        logger.error(f'No page content found for ticket {ticket}.')
-                        return
-
-                    soup = BeautifulSoup(page_content, 'html.parser')
-                    
-                    # This locate the section on the main page with all the article links
-                    for news in soup.find_all(class_='js-stream-content Pos(r)'):
-                        
-                        article = news.find('h3').find('a')
-                        title = article.get_text(strip=True)
-                        link = article.get('href')
-                        
-                    # Check if the url is about the stock an is not an ad
-                        if ticket in title:
-                            raw_articles.append({
-                                'ticket': ticket,
-                                'link': link,
-                                'title': title
-                            })
-                            
-                            logger.info(f"Successfully retrieved an articles for ticker {ticket}.")
-                            
-            logger.info(f"Total articles found: {len(raw_articles)}.")
-            return raw_articles
+        """
+        Task to retrieve news links for specific stocks from Yahoo News.
         
-        return asyncio.run(async_get_news_links_task(tickets))
+        Args:
+            tickets (list): The stock ticker symbols.
+        
+        Returns:
+            list: A list of dictionaries containing article metadata.
+        """
+        raw_articles = []
+        
+        for ticket in tickets:
+            url = f'https://finance.yahoo.com/quote/{ticket}/news/'
+            
+            # Fetch the main page using requests
+            response = requests.get(url, headers=HEADERS, cookies=COOKIES)
+            
+            if response.status_code != 200:
+                logger.error(f'Failed to retrieve content for ticket {ticket}. Status code: {response.status_code}')
+                continue
+            
+            soup = BeautifulSoup(response.text, 'html.parser')
+            
+            # Locate the section on the main page with all the article links
+            for news in soup.find_all(class_='js-stream-content Pos(r)'):
                 
+                article = news.find('h3').find('a')
+                title = article.get_text(strip=True)
+                link = article.get('href')
+                
+                # Check if the URL is about the stock and is not an ad
+                if ticket in title:
+                    raw_articles.append({
+                        'ticket': ticket,
+                        'link': link,
+                        'title': title
+                    })
+                    
+                    logger.info(f"Successfully retrieved an article for ticker {ticket}.")
+                    
+        logger.info(f"Total articles found: {len(raw_articles)}.")
+        return raw_articles
 
 
     @task(
@@ -123,62 +102,56 @@ def yahoo_extract():
         execution_timeout=timedelta(seconds=30)
     )
     def process_links_task(raw_articles):
-        async def async_process_links_task(raw_articles):
-            """
-            Task to process raw article data, extract content, and produce it to a Kafka topic.
-            
-            Args:
-                raw_articles (list): List of dictionaries containing raw article metadata.
-            """
-
-            async with aiohttp.ClientSession(headers=HEADERS, cookies=COOKIES) as session:
-
-                producer = make_producer(
-                    schema_reg_url=SCHEMA_REGISTRY_URL,
-                    bootstrap_server=BOOTSTRAP_SERVERS,
-                    schema=schemas.article_schema_v1
-                )
-                logger.info("Kafka producer created successfully.")
-                
-                for raw_article in raw_articles:
-                    article_content = await fetch_page(session, raw_article['link'])
-                    logger.info(f"Page fetched for the link {raw_article['link']}.")
-                    
-                    if article_content:
-                        article_soup = BeautifulSoup(article_content, 'html.parser')
-                        body = article_soup.find(class_="caas-body") # This find the body of the article
-                        
-                        if body:
-                            logger.info(f"Article body found for the link {raw_article['link']}.")
-                            
-                            # This return the concatenation of all the text inside the paragraphs of the article
-                            article_content = "\n".join(p.get_text(strip=False) for p in body.find_all('p'))
-                            article = Article(
-                                ticket=raw_article['ticket'],
-                                timestp=int(datetime.now().timestamp()),
-                                url=raw_article['link'],
-                                title=raw_article['title'],
-                                article_body=article_content
-                            )
-                            
-                            producer.produce(
-                                topic=TOPIC_NAME,
-                                key=article.ticket.lower(),
-                                value=article,
-                                on_delivery=ProducerCallback(article)
-                            )
-                        
-                        else:
-                            logger.warning(f"Article body NOT found for the link {raw_article['link']}.")
-
-                producer.flush()
-                logger.info("Kafka producer flushed successfully.")
+        """
+        Task to process raw article data, extract content, and produce it to a Kafka topic.
         
-        return asyncio.run(async_process_links_task(raw_articles))
+        Args:
+            raw_articles (list): List of dictionaries containing raw article metadata.
+        """
+        producer = make_producer(
+            schema_reg_url=SCHEMA_REGISTRY_URL,
+            bootstrap_server=BOOTSTRAP_SERVERS,
+            schema=schemas.article_schema_v1
+        )
+        logger.info("Kafka producer created successfully.")
+        
+        for raw_article in raw_articles:
+            response = requests.get(raw_article['link'], headers=HEADERS, cookies=COOKIES)
+            logger.info(f"Page fetched for the link {raw_article['link']}.")
+            
+            if response.status_code == 200:
+                article_soup = BeautifulSoup(response.text, 'html.parser')
+                body = article_soup.find(class_="caas-body")  # This finds the body of the article
+                
+                if body:
+                    logger.info(f"Article body found for the link {raw_article['link']}.")
+                    
+                    # This returns the concatenation of all the text inside the paragraphs of the article
+                    article_content = "\n".join(p.get_text(strip=False) for p in body.find_all('p'))
+                    article = Article(
+                        ticket=raw_article['ticket'],
+                        timestp=int(datetime.now().timestamp()),
+                        url=raw_article['link'],
+                        title=raw_article['title'],
+                        article_body=article_content
+                    )
+                    
+                    producer.produce(
+                        topic=TOPIC_NAME,
+                        key=article.ticket.lower(),
+                        value=article,
+                        on_delivery=ProducerCallback(article)
+                    )
+                
+                else:
+                    logger.warning(f"Article body NOT found for the link {raw_article['link']}.")
+            else:
+                logger.error(f"Failed to fetch the article content for link {raw_article['link']}. Status code: {response.status_code}")
 
+        producer.flush()
+        logger.info("Kafka producer flushed successfully.")
 
     articles = get_news_links_task(tickets)
     process_links_task(articles)
-
 
 yahoo_extract()
---
+++ test/fool_get_news.py
@@ -0,0 +1,117 @@
+
+from script.helper.models import Article
+import helper
+
+import logging
+from fastapi import FastAPI
+from typing import List
+
+from confluent_kafka import SerializingProducer
+from confluent_kafka.admin import AdminClient
+
+from confluent_kafka.serialization import StringSerializer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+
+import script.helper.schemas as schemas
+from script.helper.models import Article
+
+
+HEADERS = {
+'article-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.44'
+}
+TICKET = 'riot'
+PREFIXES_FOOL = [r'https://www.fool.com/quote/nasdaq/',
+                r'https://www.fool.com/quote/nyse/']
+
+BOOTSTRAP_SERVERS = "localhost:9092"
+TOPIC_NAME = "stock.news.v2"
+SCHEMA_REGISTRY_URL = "http://localhost:8081"
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+app = FastAPI()
+
+
+# helper function to create a producer
+def make_producer() -> SerializingProducer:
+    # make a SchemaRegistryClient
+    schema_reg_client = SchemaRegistryClient({'url': SCHEMA_REGISTRY_URL})
+    
+    # create AvroSerializer
+    avro_serializer = AvroSerializer(schema_reg_client,
+                                     schemas.article_schema_v1,
+                                     lambda article, ctx: article.dict())
+    
+    # create and return SerializingProducer
+    return SerializingProducer({'bootstrap.servers': BOOTSTRAP_SERVERS,
+                                'linger.ms': 300,
+                                'enable.idempotence': 'true',
+                                'acks': 'all',
+                                'key.serializer': StringSerializer('utf-8'),
+                                'value.serializer': avro_serializer,
+                                'partitioner': 'murmur2_random'})
+
+
+# class to manage callbacks when the producer writes messages to Kafka
+class ProducerCallback:
+    def __init__(self, article):
+        self.article = article
+        
+    def __call__(self, err, msg):
+        if err:
+            logger.error(f"Failed to produce article on url: {self.article.url} \nFor the stock: {self.article.ticket}", exc_info=err)
+        else:
+            logger.info(f"""
+                        __________________________________
+                        Successfully produced article on url: {self.article.url}
+                        to partition {msg.partition()}
+                        at offset {msg.offset()}
+                        __________________________________
+                        """)
+
+
+@app.post('/api/news', status_code=201, response_model=List[str])
+async def get_stock_news(ticket: str) -> List[str]:
+
+    producer = make_producer()
+    fool_links = []
+    
+    # get the links
+    for prefix in PREFIXES_FOOL:
+        try:
+            url = prefix + ticket + '/'
+            links = helper.fool_get_news_links(url, HEADERS)
+            
+            # if the function finds no link it stops
+            if links:
+                fool_links = fool_links + links
+        
+        except Exception as e:
+            print('Something whent wrong :(. ', e)
+    
+    article = helper.extract_articles(headers=HEADERS, ticket=ticket)
+    processed_urls = []
+    
+    
+    for fool_link in fool_links:
+        
+        try:
+            article = helper.fool_get_article_info(url=fool_link, headers=HEADERS, ticket=ticket)
+            
+            producer.produce(topic=TOPIC_NAME,
+                            key=article.ticket.lower(),
+                            value=article,
+                            on_delivery=ProducerCallback(article))
+            processed_urls.append(fool_link)
+            
+        except Exception as e:
+            print(f"Something went wrong while extracting the url: {fool_link}", e)
+            
+    
+    producer.flush()
+    
+    return processed_urls
