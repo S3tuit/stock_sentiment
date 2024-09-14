950ea530cbd6b6cac5812306de60c01326304167
Matteo Ladislai <maladislai@paa.it>
2024-09-14 20:58:36 +0200

    ROTATION 2 URLS MOTLEY

--- dags/helper/bs4_functions.py
+++ dags/helper/bs4_functions.py
@@ -2,32 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_soup(logger, ticket, url, **kwargs):
+def get_soup(logger, ticket, urls, **kwargs):
     """
     Fetch the content of a URL and parse it with BeautifulSoup.
         
     Args:
         ticket (str): The stock ticker symbol.
         logger (logging.Logger): An instance of the logging library.
-        url (str): the url of the page.
+        urls (list): a list of url to test.
         kwargs: Additional arguments to be used for requests.get().
         
     Returns:
         BeautifulSoup: A BeautifulSoup object containing the parsed HTML.
         
-    Raises:
-        ValueError: If 'url' is not provided in kwargs.
     """
 
-    try:
-        response = requests.get(url, **kwargs)
-        response.raise_for_status()  # Raises HTTPError for bad responses (4xx and 5xx)
-    except requests.HTTPError as http_err:
-        logger.error(f'Failed to retrieve content for ticket {ticket}. Status code: {response.status_code}. Error: {http_err}')
-        raise
-    except Exception as err:
-        logger.error(f'An error occurred for ticket {ticket}. Error: {err}')
+    success = False
+    for url in urls:
+        try:
+            response = requests.get(url, **kwargs)
+            response.raise_for_status()  # Raises HTTPError for bad responses (4xx and 5xx)
+            success = True
+            break
+        except requests.HTTPError as http_err:
+            logger.error(f'Failed to retrieve content for ticket {ticket}, url {url}. Status code: {response.status_code}. Error: {http_err}')
+            # raise
+        except Exception as err:
+            logger.error(f'An error occurred for ticket {ticket}, url {url}. Error: {err}')
+            # raise
+    
+    if success == False:
         raise
     
+    logger.info(f'Page found for url: {url}')
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
